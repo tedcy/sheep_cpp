f7e236a6b19a3c1e885445f13257ea04f56a2e09
tedcy <yue.cheng.ted@gmail.com>
2018-09-10 16:29:07 +0800

    add get unix time

--- src/extends/small_timer/small_timer_factory.cpp
+++ src/extends/small_timer/small_timer_factory.cpp
@@ -1,6 +1,7 @@
 #include "small_timer_factory.h"
 #include "asio_timer.h"
 #include "asio_timer_manager.h"
+#include <chrono>
 
 namespace small_timer{
 std::shared_ptr<TimerI> MakeTimer() {
@@ -9,5 +10,13 @@ std::shared_ptr<TimerI> MakeTimer() {
 }
 void Init() {
     AsioTimerManager::GetInstance();
+}
+uint64_t UnixTimeSecond() {
+    std::chrono::time_point<std::chrono::system_clock,std::chrono::seconds> tp = 
+        std::chrono::time_point_cast<std::chrono::seconds>(std::chrono::system_clock::now());
+    auto tmp=std::chrono::duration_cast<std::chrono::seconds>(tp.time_since_epoch());
+    std::time_t timestamp = tmp.count();
+    //std::time_t timestamp = std::chrono::system_clock::to_time_t(tp);
+    return uint64_t(timestamp);
 }
 }//namespace small_timer
