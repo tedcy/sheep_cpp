88ac5d8d5c572b4c9d0a2eb196c2e70e41c2a697
tedcy <yue.cheng.ted@gmail.com>
2018-09-27 17:55:50 +0800

    修复timer毫秒统计错误的问题

--- src/extends/small_timer/small_timer_factory.cpp
+++ src/extends/small_timer/small_timer_factory.cpp
@@ -17,4 +17,12 @@ uint64_t UnixTimeSecond() {
     //std::time_t timestamp = std::chrono::system_clock::to_time_t(tp);
     return uint64_t(timestamp);
 }
+uint64_t UnixTimeMilliSecond() {
+    std::chrono::time_point<std::chrono::system_clock,std::chrono::milliseconds> tp = 
+        std::chrono::time_point_cast<std::chrono::milliseconds>(std::chrono::system_clock::now());
+    auto tmp=std::chrono::duration_cast<std::chrono::milliseconds>(tp.time_since_epoch());
+    std::time_t timestamp = tmp.count();
+    //std::time_t timestamp = std::chrono::system_clock::to_time_t(tp);
+    return uint64_t(timestamp);
+}
 }//namespace small_timer
--- src/extends/small_timer/small_timer_factory.h
+++ src/extends/small_timer/small_timer_factory.h
@@ -5,4 +5,5 @@
 namespace small_timer{
 std::shared_ptr<TimerI> MakeTimer();
 uint64_t UnixTimeSecond();
+uint64_t UnixTimeMilliSecond();
 }//namespace small_timer
