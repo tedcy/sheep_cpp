f44931ea48050535728256f138eefde58eb344d3
tedcy <yue.cheng.ted@gmail.com>
2018-11-21 15:26:38 +0800

    ignore error of compdb can't find compile_command.json

--- init_tools/build.sh
+++ init_tools/build.sh
@@ -20,5 +20,10 @@ $cmake_path .. -DCMAKE_BUILD_TYPE=$build_type \
     -DCMAKE_TOOLCHAIN_FILE=$vcpkg_path/scripts/buildsystems/vcpkg.cmake \
     -DSHEEP_PROJECT_SOURCE_DIR=$sheep_pkg_path \
     -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+complieCommandsJsonResult=($(find . -maxdepth 1 -name compile_commands.json))
+if ((${#complieCommandsJsonResult[@]} != 0));then
+    compdb -p . list > tmp_compile_commands.json
+    mv tmp_compile_commands.json compile_commands.json
+fi
 make -j8
 cd -
